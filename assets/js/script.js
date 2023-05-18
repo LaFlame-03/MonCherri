1edf25b97373174584923a4413cd4a63a4dd4c5a
LaFlame-03 <derickfelipe2003@gmail.com>
2023-05-18 15:41:52 -0300

    Adicionando Projeto

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+var dataHoje = new Date();
+var hr = dataHoje.getHours();
+var min = dataHoje.getMinutes();
+var seg = dataHoje.getSeconds();
+
+horas.textContent = hr;
+minutos.textContent = min;
+segundos.textContent = seg;
+
+String(hr).padStart(2,0);
+String(min).padStart(2,0);
+String(seg).padStart(2,0);
+
+
+});
